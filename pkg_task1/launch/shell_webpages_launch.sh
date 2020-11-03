a6d5cf9daea9cbf03dc2db91ea46c1aa2962c3a1
Kartikey Sharma <36632816+KartikeySharma@users.noreply.github.com>
2020-11-03 12:05:20 +0530

    Initial Commit

---
+++ pkg_task1/launch/shell_webpages_launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Store URL in a variable
+URL1="http://www.hivemq.com/demos/websocket-client/"
+URL2="https://docs.google.com/spreadsheets/d/1CLWRZ1jDNNXOW5PCQBv-oCfId_jDanJXPGoOznckSQQ/edit#gid=0"
+
+# Print some message
+echo "** Opening $URL1 in Firefox **"
+echo "** Opening $URL2 in Firefox **"
+
+# Use firefox to open the URL in a new window
+firefox -new-window $URL1 
+firefox -new-window $URL2 
